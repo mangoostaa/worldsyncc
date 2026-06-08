a3af3925e0f769a618f26471928e8cade7c6c32b
KlavinAztence <eugeniovillacrespo@gmail.com>
2026-06-07 23:03:29 -0300

    Add gitignore and stop tracking build artifacts

--- build/sdk/lib/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Noxi-PC/open.mp-sdk/lib/glm
-# Build directory: C:/Users/Noxi-PC/open.mp-sdk/WorldSync/build/sdk/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/sdk/lib/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Noxi-PC/open.mp-sdk/lib/glm/glm
-# Build directory: C:/Users/Noxi-PC/open.mp-sdk/WorldSync/build/sdk/lib/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
